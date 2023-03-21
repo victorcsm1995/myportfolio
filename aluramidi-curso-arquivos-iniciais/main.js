c0d006f4b33bd0dd37cc878f0af88e1ee8234e03
Victor Moreira <78387221+victorcsm1995@users.noreply.github.com>
2023-03-21 11:01:21 -0300

    Add files via upload

---
+++ aluramidi-curso-arquivos-iniciais/main.js
@@ -0,0 +1,39 @@
+
+function tocaSomPom(seletorAudio){
+    const elemento = document.querySelector(seletorAudio);
+    
+    if(elemento != null && elemento.localName === 'audio'){
+        elemento.play();        
+    }
+    else{
+        console.log("Elemento não encontrado");
+    }
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+for(let i = 0; i < listaDeTeclas.length; i++){
+
+    const tecla = listaDeTeclas[i];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`;
+
+    tecla.onclick = function(){
+
+        tocaSomPom(idAudio);
+    }
+
+    tecla.onkeydown = function(evento){
+        
+        if(evento.code == 'Space' || evento.code == 'Enter'){
+            tecla.classList.add('ativa');
+        }
+        
+    }
+
+    tecla.onkeyup = function(){
+        tecla.classList.remove('ativa');
+    }
+}
+
+
